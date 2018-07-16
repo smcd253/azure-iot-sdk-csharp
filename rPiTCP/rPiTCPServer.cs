cef50e8acaa32494d80ad2ec7977e39e3e3308f0
Spencer McDonough <t-spmcdo@microsoft.com>
2018-07-15 19:38:36 -0700

    make server online status continuous

--- rPiTCP/rPiTCPServer.cs
+++ rPiTCP/rPiTCPServer.cs
@@ -8,25 +8,24 @@ using System.Net.Sockets;
 
 public class serv {
     public static void Main() {
-    try {
-        IPAddress ipAd = IPAddress.Parse("10.0.0.37");
-         // use local m/c IP address, and 
-         // use the same in the client
+    while(true){
+        try {
+            IPAddress ipAd = IPAddress.Parse("10.0.0.37");
+            // use local m/c IP address, and 
+            // use the same in the client
 
-/* Initializes the Listener */
-        TcpListener myList=new TcpListener(ipAd,13000);
+    /* Initializes the Listener */
+            TcpListener myList=new TcpListener(ipAd,13000);
 
-/* Start Listeneting at the specified port */        
-        myList.Start();
-        
-        Console.WriteLine("The server is running at port 13000...");    
-        Console.WriteLine("The local End point is  :" + 
-                          myList.LocalEndpoint );
-        Console.WriteLine("Waiting for a connection.....");
-        
-        Socket s;
-        while(true){
-            s=myList.AcceptSocket();
+    /* Start Listeneting at the specified port */        
+            myList.Start();
+            
+            Console.WriteLine("The server is running at port 13000...");    
+            Console.WriteLine("The local End point is  :" + 
+                            myList.LocalEndpoint );
+            Console.WriteLine("Waiting for a connection.....");
+            
+            Socket s=myList.AcceptSocket();
             Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
             
             byte[] b=new byte[100];
@@ -38,16 +37,14 @@ public class serv {
             ASCIIEncoding asen=new ASCIIEncoding();
             s.Send(asen.GetBytes("The string was recieved by the server."));
             Console.WriteLine("\nSent Acknowledgement");
-        }
-        
-/* clean up */            
-        s.Close();
-        myList.Stop();
             
+    /* clean up */            
+            myList.Stop();
+                
+        }
+        catch (Exception e) {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }    
+        }
     }
-    catch (Exception e) {
-        Console.WriteLine("Error..... " + e.StackTrace);
-    }    
-    }
-    
 }
