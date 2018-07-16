c67cd3861928a6c8d749129d970cc5ea133a1fa3
Spencer McDonough <t-spmcdo@microsoft.com>
2018-07-15 19:34:38 -0700

    make server online status continuous

--- rPiTCP/rPiTCPServer.cs
+++ rPiTCP/rPiTCPServer.cs
@@ -24,18 +24,19 @@ public class serv {
                           myList.LocalEndpoint );
         Console.WriteLine("Waiting for a connection.....");
         
-        while(Socket s=myList.AcceptSocket()){
-        Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-        
-        byte[] b=new byte[100];
-        int k=s.Receive(b);
-        Console.WriteLine("Recieved...");
-        for (int i=0;i<k;i++)
-            Console.Write(Convert.ToChar(b[i]));
+        Socket s;
+        while(s=myList.AcceptSocket()){
+            Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+            
+            byte[] b=new byte[100];
+            int k=s.Receive(b);
+            Console.WriteLine("Recieved...");
+            for (int i=0;i<k;i++)
+                Console.Write(Convert.ToChar(b[i]));
 
-        ASCIIEncoding asen=new ASCIIEncoding();
-        s.Send(asen.GetBytes("The string was recieved by the server."));
-        Console.WriteLine("\nSent Acknowledgement");
+            ASCIIEncoding asen=new ASCIIEncoding();
+            s.Send(asen.GetBytes("The string was recieved by the server."));
+            Console.WriteLine("\nSent Acknowledgement");
         }
         
 /* clean up */            
