fb286a7f763a72e6b5a4c8a4f5d260d7ea63cffe
Spencer McDonough <shmcdono@usc.edu>
2018-07-23 19:49:15 -0700

    fine tune for rpi address, push to pc for proper printing of bytes

--- rPiTCP/asyncTCPServer.cs
+++ rPiTCP/asyncTCPServer.cs
@@ -27,9 +27,10 @@ public class AsynchronousSocketListener {
         // Establish the local endpoint for the socket.  
         // The DNS name of the computer  
         // running the listener is "host.contoso.com".  
-        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());  
-        IPAddress ipAddress = ipHostInfo.AddressList[0];  
-        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);  
+        //IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());  
+        //IPAddress ipAddress = ipHostInfo.AddressList[0];  
+        IPAddress ipAddress = IPAddress.Parse("10.0.0.37");
+	IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 13000);  
 
         // Create a TCP/IP socket.  
         Socket listener = new Socket(ipAddress.AddressFamily,  
